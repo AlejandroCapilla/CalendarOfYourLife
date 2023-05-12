ec326843288513e990bb6d2f77121258ac59ea14
AlejandroCapilla <19031028@itcelaya.edu.mx>
2023-05-11 22:41:04 -0600

    Creada interfaz para modificar objetivo

---
+++ app/src/main/java/com/example/calendarofyourlife/ModificarObjetivoActivity.java
@@ -0,0 +1,14 @@
+package com.example.calendarofyourlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModificarObjetivoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_objetivo);
+    }
+}
